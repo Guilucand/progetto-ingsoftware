811f4e1c2cea4e5a361be510e950a577807810a2
littexsimo <simolitterini@gmail.com>
2018-07-01 21:22:52 +0200

    aggiunto ER

--- src/main/java/it/ingsoftw/progetto/server/ServerConfig.java
+++ src/main/java/it/ingsoftw/progetto/server/ServerConfig.java
@@ -13,5 +13,5 @@ public final class ServerConfig {
     /**
      * Il nome DNS del server
      */
-    public static String hostname = "localhost";//"192.168.1.72";
+    public static String hostname = "localhost";//192.168.1.75";//"192.168.1.72";
 }
